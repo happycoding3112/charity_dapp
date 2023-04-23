3cc9d45ba37020cea9f08de01dff009fc9153b47
happycoding3112 <84672983+happycoding3112@users.noreply.github.com>
2023-04-23 15:36:08 +0530

    admin utility components

---
+++ client/src/components/ViewFile.jsx
@@ -0,0 +1,32 @@
+import { Viewer } from '@react-pdf-viewer/core'
+import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+import { FaTimes } from 'react-icons/fa';
+import { useNavigate, useParams } from 'react-router-dom';
+
+const ViewFile = () => {
+
+  const { doc } = useParams();
+
+  const navigate = useNavigate();
+
+  const defaultLayoutPluginInstance = defaultLayoutPlugin();
+
+  const onClose = () => {
+    navigate("/admin");
+  }
+
+  return (
+    <div className='fixed top-0 left-0 flex justify-center items-center h-screen w-screen py-4'>
+      <div className='h-full w-3/5 bg-gray-700 rounded-md pb-10'>
+        <div className='w-full flex justify-between items-center p-2 text-white'>
+          <p>View Supporting Document</p>
+          <FaTimes className='cursor-pointer' onClick={onClose} />
+        </div>
+        <Viewer plugins={[defaultLayoutPluginInstance]} fileUrl={"/uploads/" + doc} />
+      </div>
+    </div>
+  )
+}
+
+export default ViewFile;
