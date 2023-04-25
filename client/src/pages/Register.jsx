b247c2381d5324e8015a2e264f3ffac942cd7809
happycoding3112 <84672983+happycoding3112@users.noreply.github.com>
2023-04-25 15:45:53 +0530

    small changes to registration form

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -77,34 +77,36 @@ const Register = () => {
       className="`fixed top-0 left-0 w-screen h-screen flex items-center justify-center bg-gray-900 bg-opacity-50 transform transition-transform duration-300 scale-100"
     >
       <div className="bg-white rounded-xl w-11/12 h-7/12 md:w-2/6 p-6">
-        <form onSubmit={handleSubmit} className="flex flex-col mb-2">
+        <form onSubmit={handleSubmit} className="flex flex-col mb-4">
           <div className="flex justify-between items-center">
-            <p>Register NGO</p>
+            <p className="font-medium">Register NGO</p>
             <button onClick={onClose}>
               <FaTimes />
             </button>
           </div>
 
-          <div className="flex justify-between items-center bg-gray-200 rounded-md mt-5">
-            <input
-              className="block w-full border-0 bg-transparent p-3 focus:outline-none focus:ring-0 text-sm text-slate-500"
-              type="text"
-              name="name"
-              onChange={handleChange}
-              placeholder="NGO Name"
-              required
-            />
-          </div>
+          <div className="flex justify-between items-center rounded-md mt-5 gap-3">
+            <div className="bg-gray-200 w-1/2 rounded-md">
+              <input
+                className="block w-full border-0 bg-transparent p-3 focus:outline-none focus:ring-0 text-sm text-slate-500"
+                type="text"
+                name="name"
+                onChange={handleChange}
+                placeholder="NGO Name"
+                required
+              />
+            </div>
 
-          <div className="flex justify-between items-center bg-gray-200 rounded-md mt-5">
-            <input
-              className="block w-full border-0 bg-transparent p-3 focus:outline-none focus:ring-0 text-sm text-slate-500"
-              type="email"
-              name="email"
-              onChange={handleChange}
-              placeholder="NGO Email"
-              required
-            />
+            <div className="bg-gray-200 w-1/2 rounded-md">
+              <input
+                className="block w-full border-0 bg-transparent p-3 focus:outline-none focus:ring-0 text-sm text-slate-500"
+                type="email"
+                name="email"
+                onChange={handleChange}
+                placeholder="NGO Email"
+                required
+              />
+            </div>
           </div>
 
           <div className="flex justify-between items-center bg-gray-200 rounded-md mt-5 px-4">
@@ -179,24 +181,26 @@ const Register = () => {
             ></textarea>
           </div>
 
-          <div className="flex justify-between items-center bg-gray-200 rounded-md mt-5">
-            <input
-              className="block w-full border-0 bg-transparent p-3 focus:outline-none focus:ring-0 text-sm text-slate-500" type="password"
-              name="password"
-              onChange={handleChange}
-              placeholder="Set Password"
-              required
-            />
-          </div>
+          <div className="flex justify-between items-center rounded-md mt-5 gap-3">
+            <div className="w-1/2 rounded-md bg-gray-200">
+              <input
+                className="block w-full border-0 bg-transparent p-3 focus:outline-none focus:ring-0 text-sm text-slate-500" type="password"
+                name="password"
+                onChange={handleChange}
+                placeholder="Set Password"
+                required
+              />
+            </div>
 
-          <div className="flex justify-between items-center bg-gray-200 rounded-md mt-5">
-            <input
-              className="block w-full border-0 bg-transparent p-3 focus:outline-none focus:ring-0 text-sm text-slate-500" type="password"
-              name="confirmPassword"
-              onChange={handleChange}
-              placeholder="Confirm Password"
-              required
-            />
+            <div className="w-1/2 rounded-md bg-gray-200">
+              <input
+                className="block w-full border-0 bg-transparent p-3 focus:outline-none focus:ring-0 text-sm text-slate-500" type="password"
+                name="confirmPassword"
+                onChange={handleChange}
+                placeholder="Confirm Password"
+                required
+              />
+            </div>
           </div>
 
           <button className="mt-5 leading-tight shadow-md uppercase font-semibold text-sm inline-block text-white bg-green-500 hover:bg-green-600 rounded-md px-6 py-2.5">
@@ -206,7 +210,11 @@ const Register = () => {
 
         <div className="flex flex-col gap-1">
           <span className="font-semibold text-sm">Already Registered?</span>
-          <Link to={"/ngoLogin"}><span className="font-semibold text-sm hover:text-blue-700 hover:underline">Click Here to go to Login Page</span></Link>
+          <span className="font-semibold text-sm">
+            <Link className="hover:text-blue-700 hover:underline" to={"/ngoLogin"}>
+              Click Here to go to Login Page
+            </Link>
+          </span>
         </div>
 
       </div>
