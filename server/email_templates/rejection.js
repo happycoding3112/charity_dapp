3e208f0e552d89a8d1441a49ec9f099d8cee0130
happycoding3112 <84672983+happycoding3112@users.noreply.github.com>
2023-05-04 23:21:31 +0530

    template for rejection email

---
+++ server/email_templates/rejection.js
@@ -0,0 +1,28 @@
+const rejectionMsg = `
+
+<div>
+  <h1 style="text-align:center" >Better Luck Next Time!</h1>
+ </div>
+
+ <div style="display: block; margin-left: auto; margin-right: auto; width: 34%">
+  <img style="object-fit: cover" height="250px" width="400px" src="https://c7.alamy.com/comp/FCM325/application-denied-grunge-rubber-stamp-on-white-background-vector-FCM325.jpg" />
+ </div>
+
+ <div>
+  <h1 style="color: red; text-align:center">Sorry! But Your NGO application has been rejected!</h1>
+ </div>
+
+<div style="text-align: center; font-size: 18px">
+  <p>After a thorough background check on your NGO details, we regret to inform you that your NGO did not meet the necessary requirements to be a part of our platform</p>
+  <p>You can try registering again with documents that will help in better understanding and verifying your NGO</p>
+  <a href="http://localhost:3000/registerNGO">Click here to Register again</a>
+</div>
+
+<div style="padding:0 200px">
+  <p>From Cause✌️Care,</p>
+  <p>Keep trying!</p>
+</div>
+
+`;
+
+module.exports = { rejectionMsg };
