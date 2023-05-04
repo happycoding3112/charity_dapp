feac6bf1304e39d97b4d56b7d3e894f5e3487e84
piyusha2001 <ppiyusha2001@gmail.com>
2023-05-04 23:19:45 +0530

    template for approval email

---
+++ server/email_templates/approval.js
@@ -0,0 +1,27 @@
+const approvalMsg = `
+ <div>
+  <h1 style="text-align:center" >Let's make a better world :)</h1>
+ </div>
+
+ <div style="display: block; margin-left: auto; margin-right: auto; width: 44%">
+  <img style="object-fit: cover" height="250px" width="500px" src="https://cdn.dribbble.com/users/4358240/screenshots/14825308/media/84f51703b2bfc69f7e8bb066897e26e0.gif" />
+ </div>
+
+ <div>
+  <h1 style="color: orange; text-align:center">Congratulations! Your NGO has been approved!</h1>
+ </div>
+
+<div style="text-align: center; font-size: 18px">
+  <p>After a thorough background check on your NGO details, we are happy to inform that your NGO has been approved!</p>
+  <p>You can now login to Cause✌️Care and create and manage your Campaigns!</p>
+  <p>Welcome to Cause✌️Care family</p>
+  <a href="http://localhost:3000/ngoLogin">Click here to Login</a>
+</div>
+
+<div style="padding:0 200px">
+  <p>From Cause✌️Care,</p>
+  <p>Keep up the Good Work!</p>
+</div>
+`;
+
+module.exports = { approvalMsg };
