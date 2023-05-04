a9b28bc6963eb23f58d4372ba9add575af0be8be
happycoding3112 <84672983+happycoding3112@users.noreply.github.com>
2023-05-04 23:23:48 +0530

    small changes

--- client/src/components/Ngo.jsx
+++ client/src/components/Ngo.jsx
@@ -6,13 +6,11 @@ import { toast } from "react-toastify";
 
 const Ngo = ({ ngo }) => {
 
-  const approveNGO = async (ngoID) => {
+  const approveNGO = async (ngoID, ngoEmail) => {
     try {
       const url = "http://localhost:8080/api/admin/approveNGO";
 
-      const data = { id: ngoID }
-
-      console.log(data)
+      const data = { id: ngoID, email: ngoEmail }
 
       const { data: res, status } = await axios.put(url, data);
 
@@ -28,16 +26,13 @@ const Ngo = ({ ngo }) => {
     } catch (err) {
       console.log(err)
     }
-
   }
 
-  const rejectNGO = async (ngoID) => {
+  const rejectNGO = async (ngoID, ngoEmail) => {
     try {
       const url = "http://localhost:8080/api/admin/rejectNgo";
 
-      const data = { id: ngoID }
-
-      console.log(data)
+      const data = { id: ngoID, email: ngoEmail }
 
       const { data: res, status } = await axios.post(url, data);
 
@@ -61,41 +56,46 @@ const Ngo = ({ ngo }) => {
   }
 
   return (
-    <tr className="border-b border-green-200">
-      <td className="text-sm font-medium px-6 py-4 whitespace-nowrap border-2 border-green-200">
-        <p>{ngo.name}</p>
-      </td>
-      <td className="text-sm font-medium px-6 py-4 whitespace-nowrap border-2 border-green-200">
-        {
-          isPDF(ngo.proof) ?
-            (<Link to={"/admin/viewPdf/" + ngo?.proof}><p className="cursor-pointer">{truncate(ngo.proof, 5, 4, 11)}</p></Link>)
-            :
-            (<Link to={"/admin/viewImage/" + ngo?.proof}><p className="cursor-pointer">{truncate(ngo.proof, 5, 4, 11)}</p></Link>)
-        }
-      </td>
-      <td className="text-sm font-medium px-6 py-4 whitespace-nowrap border-2 border-green-200">
-        {
-          isPDF(ngo.annualReport) ?
-            (<Link to={"/admin/viewPdf/" + ngo?.annualReport}><p className="cursor-pointer">{truncate(ngo.annualReport, 5, 4, 11)}</p></Link>)
-            :
-            (<Link to={"/admin/viewImage/" + ngo?.annualReport}><p className="cursor-pointer">{truncate(ngo.annualReport, 5, 4, 11)}</p></Link>)
-        }
-      </td>
-      <td className="text-sm text-white font-semibold px-6 py-4 whitespace-nowrap border-2 border-green-200">
-        {
-          ngo.isVerified === "true" ? (
-            <div>
-              <p className="text-center text-green-500">Verified</p>
-            </div>
-          ) : (
-            <div className="flex justify-between items-center">
-              <p onClick={() => approveNGO(ngo.id)} className="flex justify-center items-center bg-green-500 h-10 w-10 rounded-full cursor-pointer">Yes</p>
-              <p onClick={() => rejectNGO(ngo.id)} className="flex justify-center items-center bg-red-600 h-10 w-10 rounded-full cursor-pointer">No</p>
-            </div>
-          )
-        }
-      </td>
-    </tr >
+    <>
+      {
+        ngo.role == "ngo" ? (
+          <tr className="border-b border-green-200">
+            <td className="text-sm font-medium px-6 py-4 whitespace-nowrap border-2 border-green-200">
+              <p>{ngo.name}</p>
+            </td>
+            <td className="text-sm font-medium px-6 py-4 whitespace-nowrap border-2 border-green-200">
+              {
+                isPDF(ngo.proof) ?
+                  (<Link to={"/admin/viewPdf/" + ngo?.proof}><p className="cursor-pointer">{truncate(ngo.proof, 5, 4, 11)}</p></Link>)
+                  :
+                  (<Link to={"/admin/viewImage/" + ngo?.proof}><p className="cursor-pointer">{truncate(ngo.proof, 5, 4, 11)}</p></Link>)
+              }
+            </td>
+            <td className="text-sm font-medium px-6 py-4 whitespace-nowrap border-2 border-green-200">
+              {
+                isPDF(ngo.annualReport) ?
+                  (<Link to={"/admin/viewPdf/" + ngo?.annualReport}><p className="cursor-pointer">{truncate(ngo.annualReport, 5, 4, 11)}</p></Link>)
+                  :
+                  (<Link to={"/admin/viewImage/" + ngo?.annualReport}><p className="cursor-pointer">{truncate(ngo.annualReport, 5, 4, 11)}</p></Link>)
+              }
+            </td>
+            <td className="text-sm text-white font-semibold px-6 py-4 whitespace-nowrap border-2 border-green-200">
+              {
+                ngo.isVerified === "true" ? (
+                  <div>
+                    <p className="text-center text-green-500">Verified</p>
+                  </div>
+                ) : (
+                  <div className="flex justify-between items-center">
+                    <p onClick={() => approveNGO(ngo.id, ngo.email)} className="flex justify-center items-center bg-green-500 h-10 w-10 rounded-full cursor-pointer">Yes</p>
+                    <p onClick={() => rejectNGO(ngo.id, ngo.email)} className="flex justify-center items-center bg-red-600 h-10 w-10 rounded-full cursor-pointer">No</p>
+                  </div>
+                )
+              }
+            </td>
+          </tr >) : null
+      }
+    </>
   )
 }
 
