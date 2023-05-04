8a0c3de2e742d656d656a03b756c39a7d1d6a356
happycoding3112 <84672983+happycoding3112@users.noreply.github.com>
2023-05-04 23:20:12 +0530

    template for rejection email

---
+++ server/email_templates/registration.js
@@ -0,0 +1,27 @@
+
+const msg = `
+ <div>
+  <h1 style="text-align:center" >Let's make a better world :)</h1>
+ </div>
+
+ <div style="display: block; margin-left: auto; margin-right: auto; width: 50%">
+  <img height="200px" src="https://www.careerguide.com/career/wp-content/uploads/2023/04/AdobeStock_267521221-1024x379.jpeg" />
+ </div>
+
+ <div>
+  <h1 style="color: orange; text-align:center">Thank you for registering!</h1>
+ </div>
+
+<div style="text-align: center; font-size: 18px">
+  <p>We have received the details of your Organization and are currently under verification</p>
+  <p>Kindly wait for the verification process to complete!</p>
+  <p>You will be informed about it via mail!</p>
+</div>
+
+<div style="padding:0 200px">
+  <p>From Cause✌️Care,</p>
+  <p>Stay Tuned!</p>
+</div>
+`;
+
+module.exports = { msg };
