0969bff5518c9669afa8d431a787f16d08bf5bd7
happycoding3112 <84672983+happycoding3112@users.noreply.github.com>
2023-05-04 23:19:10 +0530

    added nodemailer code for sending emails

--- server/controllers/register.js
+++ server/controllers/register.js
@@ -1,6 +1,8 @@
 const { con } = require("../config/dbConnection.js");
 const bcrypt = require("bcrypt");
 const salt = bcrypt.genSaltSync(10);
+const sendEmail = require("../utils/sendEmail.js");
+const { msg } = require("../email_templates/registration.js");
 
 const registerNGO = (req, res) => {
   const q = "SELECT * FROM ngo WHERE name = (?) ";
@@ -9,14 +11,10 @@ const registerNGO = (req, res) => {
     if (err) return res.status(500).json(err.message);
     if (data?.length) return res.status(409).json("NGO is already registered.");
 
-    console.log("Hello!");
-
     const hashedPassword = bcrypt.hashSync(req.body.password, salt);
 
     const q =
-      "INSERT INTO ngo (`name`, `email`, `establishedDate`, `proof`, `annualReport`, `description`, `password`) VALUES (?)";
-
-    console.log("User Input", req.body);
+      "INSERT INTO ngo (`name`, `email`, `establishedDate`, `proof`, `annualReport`, `ngoAddress`, `password`) VALUES (?)";
 
     const values = [
       req.body.name,
@@ -24,14 +22,13 @@ const registerNGO = (req, res) => {
       req.body.establishedDate,
       req.body.proof,
       req.body.annualReport,
-      req.body.description,
+      req.body.ngoAddress,
       hashedPassword,
     ];
 
     con.query(q, [values], (err, data) => {
       if (err) return res.status(500).json(err);
-      console.log("Data inserted");
-      console.log(data);
+      sendEmail(req.body.email, "Cause✌️Care - NGO Registered", "HELLO!", msg);
       return res.status(200).send({ message: "NGO Registered successfully" });
     });
   });
