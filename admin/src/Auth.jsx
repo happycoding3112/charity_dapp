d84faf5d8ab89f4298592f0d40cd3c7fdac93ac0
piyusha2001 <ppiyusha2001@gmail.com>
2023-05-20 11:00:45 +0530

    logic for admin authentication

---
+++ admin/src/Auth.jsx
@@ -0,0 +1,29 @@
+/* eslint-disable react/prop-types */
+import { useEffect, useState } from "react"
+import { Navigate } from "react-router-dom";
+
+const Auth = ({ children }) => {
+
+  const [currentUser, setCurrentUser] = useState(undefined);
+
+  useEffect(() => {
+    const fetchUser = () => {
+      if (localStorage.getItem("authToken")) {
+        const userData = localStorage.getItem("user");
+        console.log(userData);
+        setCurrentUser(userData);
+      } else {
+        setCurrentUser(null)
+      }
+    }
+    fetchUser();
+  }, [])
+
+  if (currentUser === undefined) return null
+
+  if (!currentUser) return <Navigate to="/" />
+
+  return children
+}
+
+export default Auth
