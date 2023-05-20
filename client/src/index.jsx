2224722cb2826ebf695dd76c0954701944ca49b1
happycoding3112 <84672983+happycoding3112@users.noreply.github.com>
2023-05-20 12:02:26 +0530

    small changes to the index file

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -6,8 +6,10 @@ import 'react-toastify/dist/ReactToastify.css'
 import App from './App'
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
   document.getElementById('root')
 )
