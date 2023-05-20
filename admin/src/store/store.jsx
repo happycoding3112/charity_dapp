9735fc362f0cbb25508a1e850dc3a12583528d2a
piyusha2001 <ppiyusha2001@gmail.com>
2023-05-20 10:59:09 +0530

    global store for managing state of various modals

---
+++ admin/src/store/store.jsx
@@ -0,0 +1,24 @@
+import { createGlobalState } from "react-hooks-global-state";
+
+const { setGlobalState, useGlobalState, getGlobalState } = createGlobalState({
+  loginModal: "scale-100",
+});
+
+const truncate = (text, startChars, endChars, maxLength) => {
+  if (text.length > maxLength) {
+    let start = text.substring(0, startChars);
+    let end = text.substring(text.length - endChars, text.length);
+    while (start.length + end.length < maxLength) {
+      start = start + ".";
+    }
+    return start + end;
+  }
+  return text;
+};
+
+export {
+  useGlobalState,
+  setGlobalState,
+  getGlobalState,
+  truncate,
+};
