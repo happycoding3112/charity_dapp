7c7cf3e41721a501cc58994da458ada244280dda
happycoding3112 <84672983+happycoding3112@users.noreply.github.com>
2023-05-20 11:08:28 +0530

    final changes to the card component

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -3,17 +3,26 @@ import Identicons from "react-identicons";
 import { daysRemaining, truncate } from "../store";
 import { FaEthereum } from "react-icons/fa";
 
-const Card = ({ campaign }) => {
+const Card = ({ campaign, title }) => {
+
+  const expired = new Date().getTime() > Number(campaign?.deadline + '000')
+
   return (
-    <div id="campaigns" className="flex bg-white rounded-lg hover:shadow-lg hover:shadow-green-300 w-64 m-4">
+    <div id="campaigns" style={{ maxHeight: "444.8px" }} className="flex bg-white rounded-lg hover:shadow-lg hover:shadow-green-300 w-64 m-4">
       <Link to={"/campaigns/" + campaign?.id}>
         <img
           className="rounded-lg h-64 w-full object-cover"
-          src={campaign?.imageURL}
-          alt="project_title"
+          src={"/clientUploads/" + campaign?.imageURL}
+          alt={"/clientUploads/" + campaign?.imageURL}
         />
         <div className="p-4 text-gray-700">
-          <h1 className="mb-2">{campaign?.title}</h1>
+          <h1 className="mb-2">
+            {
+              (title?.length > 20) ? (
+                title.substring(0, 20) + "..."
+              ) : (title)
+            }
+          </h1>
           <div className="flex flex-col">
             <div className="flex justify-between items-center mb-3">
               <Identicons
@@ -23,7 +32,7 @@ const Card = ({ campaign }) => {
               />
               <small>{truncate(campaign?.owner, 4, 4, 11)}</small>
             </div>
-            <small className="text-gray-500 mb-2">{new Date().getTime() > Number(campaign.deadline + "000") ? "Expired" : daysRemaining(campaign.deadline)}{" "} left</small>
+            <small className="text-gray-500 mb-2">{new Date().getTime() > Number(campaign.deadline + "000") ? "Expired" : daysRemaining(campaign.deadline) + "left"}</small>
           </div>
           <div className="w-full bg-gray-300">
             <div
@@ -42,7 +51,19 @@ const Card = ({ campaign }) => {
 
           <div className="flex justify-between items-center flex-wrap text-gray-500 font-bold mt-4">
             <small>{campaign.donors} Donation{campaign.donors == 1 ? "" : "s"}</small>
-            <small className="text-green-500">Open</small>
+            {expired ? (
+              <small className="text-red-500">Expired</small>
+            ) : campaign?.status == 0 ? (
+              <small className="text-green-500">Open</small>
+            ) : campaign?.status == 1 ? (
+              <small className="text-green-500">Accepted</small>
+            ) : campaign?.status == 2 ? (
+              <small className="text-gray-500">Reverted</small>
+            ) : campaign?.status == 3 ? (
+              <small className="text-red-500">Deleted</small>
+            ) : (
+              <small className="text-orange-500">Completed</small>
+            )}
           </div>
         </div>
       </Link>
