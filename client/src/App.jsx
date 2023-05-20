0c67a46b9633bbefbef5ad175947dc0b89cab57f
happycoding3112 <84672983+happycoding3112@users.noreply.github.com>
2023-05-20 12:02:58 +0530

    made major changes to the app.jsx file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,38 +1,62 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { Worker } from "@react-pdf-viewer/core";
-import Home from "./pages/Home";
-import Campaign from "./pages/Campaign";
 import { useEffect, useState } from "react";
 import { isWalletConnected } from "./services/blockchain";
 import { ToastContainer } from "react-toastify";
+import Home from "./pages/Home";
+import Campaign from "./pages/Campaign";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
-import Admin from "./pages/Admin";
-import ViewFile from "./components/ViewFile";
-import ViewImg from "./components/ViewImg";
+import Navbar from "./components/Navbar"
+import NgoHome from "./pages/NgoHome";
+import ViewCampaigs from "./pages/ViewCampaigs";
 
 const App = () => {
   const [loaded, setLoaded] = useState(false)
-  useEffect(async () => {
-    await isWalletConnected();
-    setLoaded(true);
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const walletConnection = async () => {
+      await isWalletConnected();
+      setLoaded(true);
+    }
+
+    if (localStorage.getItem("authToken")) setLoggedIn(true)
+    walletConnection()
   }, [])
+
   return (
     <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
       <div className="min-h-screen relative bg-gray-900">
-        {loaded ? (<Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/campaigns/:id" element={<Campaign />} />
-          <Route path="/registerNGO" element={<Register />} />
-          <Route path="/ngoLogin" element={<Login />} />
-          <Route path="/admin" element={<Admin />} />
-          <Route path="/admin/viewPdf/:doc" element={<ViewFile />} />
-          <Route path="/admin/viewImage/:img" element={<ViewImg />} />
-        </Routes>) : null}
+        <Navbar />
+        {loaded ? (
+          <Routes>
+            {
+              loggedIn
+                ?
+                (
+                  <Route path="/" element={
+                    <NgoHome />
+                  }
+                  />
+                )
+                :
+                (
+                  <Route path="/" element={
+                    <Home />
+                  }
+                  />
+                )}
+            <Route path="/campaigns/:id" element={<Campaign />} />
+            <Route path="/registerNGO" element={<Register />} />
+            <Route path="/ngoLogin" element={<Login />} />
+            <Route path="/viewCamps" element={<ViewCampaigs />} />
+          </Routes>
+        ) : null}
         <ToastContainer
           position="bottom-center"
-          autoClose={2000}
+          autoClose={4000}
           hideProgressBar={false}
           newestOnTop={false}
           closeOnClick
