63fe9a552b91014c674d05ccc48072dd19726de3
piyusha2001 <ppiyusha2001@gmail.com>
2023-06-01 20:14:56 +0530

    added mysql2 and removed mysql

--- server/config/dbConnection.js
+++ server/config/dbConnection.js
@@ -1,11 +1,11 @@
-const mysql = require("mysql");
+const mysql = require('mysql2');
 const { DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME } = process.env;
 
 const con = mysql.createConnection({
-  host: DB_HOST,
-  user: DB_USERNAME,
-  password: DB_PASSWORD,
-  database: DB_NAME,
+	host: DB_HOST,
+	user: DB_USERNAME,
+	password: DB_PASSWORD,
+	database: DB_NAME,
 });
 
 module.exports = { con };
